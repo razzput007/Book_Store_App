39b3a5551a5e3abaa17a775abbfac82619e23359
Raja Kumar <dhoom.rk74@gmail.com>
2023-12-19 18:10:48 +0530

    Creating Data Folder

---
+++ BookStoreApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookStoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
