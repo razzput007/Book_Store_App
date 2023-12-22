7b7e9c003f7585be41b7611c101421703b482897
Raja Kumar <dhoom.rk74@gmail.com>
2023-12-22 18:05:14 +0530

    Crud Operation

--- BookStoreApp/Controllers/CategoryController.cs
+++ BookStoreApp/Controllers/CategoryController.cs
@@ -22,21 +22,88 @@ namespace BookStoreApp.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            //if (obj.Name == obj.DisplayOrder.ToString()) {
-            //    ModelState.AddModelError("name", "The Display Order name cannot be same as categoty name ");
-            //}
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "the display order name cannot be same as categoty name ");
+            }
             //if (obj.Name!=null &&  obj.Name.ToLower() == "test")
             //{
             //    ModelState.AddModelError("", "The value is invalid");
             //}
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully";
                 return RedirectToAction("Index");
             }
             return View();
            
+        }
+        public IActionResult Edit(int ? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+         
+            //if (obj.Name!=null &&  obj.Name.ToLower() == "test")
+            //{
+            //    ModelState.AddModelError("", "The value is invalid");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int ? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+           ModelState.AddModelError("", "The value is invalid");
+           
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+
         }
     }
 }
