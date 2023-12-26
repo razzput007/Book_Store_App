cdf7e042d8fcd6b0af63516122074bc948977110
Raja Kumar <dhoom.rk74@gmail.com>
2023-12-26 18:17:36 +0530

    Adding more validation

--- BookStoreApp/Models/Category.cs
+++ BookStoreApp/Models/Category.cs
@@ -8,6 +8,8 @@ namespace BookStoreApp.Models
         public int Id { get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [RegularExpression("^[a-zA-Z]+$",ErrorMessage="Name should be only Characters")]
+        [StringLength(20,MinimumLength =5,ErrorMessage ="Name should be between 5-20")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
         [Range(1,100,ErrorMessage ="Display Order must be between 1-100")]
